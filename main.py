5b5fedaba1f5308e8ce1e318c21f3120b897c483
Mir Habeel Ahmad <102403483+Habeel06@users.noreply.github.com>
2022-11-15 11:46:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-x=int(input("Length of the password:"))
+x=int(input("Length of the password (it should be less than 35) :"))
 a="abcdefghijklmnopqrstuvwxyz"
 b="1234567890"
-print("".join(random.sample(a+b,36)))
+print("".join(random.sample(a+b,x)))
