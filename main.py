0fc8a18f9e108894c9b7441ffa5b332af039a8d6
Mir Habeel Ahmad <102403483+Habeel06@users.noreply.github.com>
2022-11-15 14:59:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ while True:
             
                         a="abcdefghijklmnopqrstuvwxyz"
                         b="1234567890"
-                        print("".join(random.sample(a+b,y)))
+                         c="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                        print("".join(random.sample(a+b+c,y)))
             
 
             
         else:
             print("Try Again-The length should be less than or equal to 35")
     except ValueError:
-        print("Length should be a whole number which is between 0-35. TRY AGAIN!")
+          print("Length should be a whole number which is between 8-35 (recommended). TRY AGAIN!")
