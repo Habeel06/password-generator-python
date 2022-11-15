feac7b2d1801f014a256e20a26920a7477feaefa
Mir Habeel Ahmad <102403483+Habeel06@users.noreply.github.com>
2022-11-15 14:55:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import random
-while True:
-    x=int(input("Length of the password (it should be less than 35) :"))
-    if x <= 35:
-        a="abcdefghijklmnopqrstuvwxyz"
-        b="1234567890"
-        print("".join(random.sample(a+b,x)))
 
-    else:
-        print("Try Again-The length should be less than or equal to 35")
+while True:
+
+    x=input("Length of the password (it should be less than 35) :")
+    
+    try:
+        y = int(x) 
+        if y <= 35:
+            
+                        a="abcdefghijklmnopqrstuvwxyz"
+                        b="1234567890"
+                        print("".join(random.sample(a+b,y)))
+            
+
+            
+        else:
+            print("Try Again-The length should be less than or equal to 35")
+    except ValueError:
+        print("Length should be a whole number which is between 0-35. TRY AGAIN!")
